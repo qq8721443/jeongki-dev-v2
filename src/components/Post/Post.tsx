c602ca19d7578b5f6ff251560230b029ae6bd711
qq8721443 <qq8721443@naver.com>
2022-10-18 02:23:52 +0900

    feat: Post 컴포넌트 및 스토리 추가

---
+++ src/components/Post/Post.tsx
@@ -0,0 +1,83 @@
+import * as React from 'react';
+import styled from '@emotion/styled';
+
+import { CgCalendarDates } from 'react-icons/cg';
+import { BiTimeFive } from 'react-icons/bi';
+
+interface PostProps {
+  title?: string;
+  content?: string;
+  date?: string;
+  readTime?: string;
+  width?: number | string;
+}
+
+const PostContainer = styled.div<PostProps>`
+  position: relative;
+  box-sizing: border-box;
+  width: ${(props) => props.width};
+  padding: 8px;
+  border-radius: 5px;
+  background-color: transparent;
+  cursor: pointer;
+  &:hover {
+    background-color: #efefef;
+  }
+`;
+
+const Title = styled.h2`
+  margin-top: 0;
+  font-size: 1.25rem;
+`;
+
+const Content = styled.div`
+  color: #bcbcbc;
+`;
+
+const Additional = styled.div`
+  position: relative;
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  width: 100%;
+  padding-top: 8px;
+  color: #bcbcbc;
+  font-size: 14px;
+`;
+
+const VerticalAlign = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 4px;
+`;
+
+const Post = ({
+  title = 'Post Title',
+  content = 'Post Content',
+  date = '2022-02-02',
+  readTime = '3 min read',
+  width = 300,
+}: PostProps) => {
+  if (typeof width === 'number') {
+    width = width + 'px';
+  }
+
+  return (
+    <PostContainer width={width}>
+      <Title>{title}</Title>
+      <Content>{content}</Content>
+      <Additional>
+        <VerticalAlign>
+          <CgCalendarDates />
+          {date}
+        </VerticalAlign>
+        <VerticalAlign>
+          <BiTimeFive />
+          {readTime}
+        </VerticalAlign>
+      </Additional>
+    </PostContainer>
+  );
+};
+
+export { Post };
