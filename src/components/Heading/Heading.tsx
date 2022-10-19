70bbde51bbce8aa3de779226619e9a7523e1baef
qq8721443 <qq8721443@naver.com>
2022-10-19 23:58:05 +0900

    feat: Heading 컴포넌트 및 스토리 추가

---
+++ src/components/Heading/Heading.tsx
@@ -0,0 +1,62 @@
+import * as React from 'react';
+import styled from '@emotion/styled';
+
+import { AiOutlineLink } from 'react-icons/ai';
+
+const Wrapper = styled.div`
+  position: relative;
+  width: fit-content;
+  cursor: pointer;
+  &:hover {
+    color: #bcbcbc;
+  }
+`;
+
+const IconWrapper = styled.div`
+  position: absolute;
+  display: flex;
+  align-items: center;
+  left: 0;
+  top: 50%;
+  transform: translate(-100%, -60%);
+`;
+
+interface HeadingProps {
+  children: React.ReactNode;
+  level: number;
+  icon?: boolean;
+  onClick: () => void;
+}
+
+/**
+ * 필요한 것
+ * @params level
+ * @params icon
+ * @params children
+ */
+const Heading = ({ children, level, icon = false, onClick }: HeadingProps) => {
+  const [isHover, setIsHover] = React.useState<boolean>(false);
+
+  const Tag = `h${level}` as keyof JSX.IntrinsicElements;
+
+  const handleClick = () => {
+    onClick?.();
+  };
+
+  return (
+    <Wrapper
+      onClick={handleClick}
+      onMouseOver={() => setIsHover(true)}
+      onMouseOut={() => setIsHover(false)}
+    >
+      {icon && isHover && (
+        <IconWrapper>
+          <AiOutlineLink />
+        </IconWrapper>
+      )}
+      <Tag>{children}</Tag>
+    </Wrapper>
+  );
+};
+
+export { Heading };
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
 export * from './ToggleButton';
 export * from './Post';
+export * from './Heading';
