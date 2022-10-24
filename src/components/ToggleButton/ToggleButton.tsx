2b9ce5b4e9830400a09c5b161b92ef87841caf17
qq8721443 <qq8721443@naver.com>
2022-10-24 19:50:17 +0900

    refactor: inner shadow 추가 및 기본 색상 변경

--- src/components/ToggleButton/ToggleButton.tsx
+++ src/components/ToggleButton/ToggleButton.tsx
@@ -18,6 +18,7 @@ const ToggleContainer = styled.div<ToggleContainerProps>`
   padding: ${(props) => props.padding};
   border-radius: ${(props) => props.radius};
   background-color: gray;
+  box-shadow: inset 0 0 4px rgba(0, 0, 0, 0.25);
   cursor: pointer;
   transition: background-color 0.3s ease;
 `;
@@ -44,7 +45,7 @@ const ToggleButton = ({
   width = 100,
   height = 30,
   padding = 3,
-  color = ['green', 'red'],
+  color = ['#7699F5', '#FAA352'],
   onClick = () => console.warn('Please add onClick handler function.'),
 }: ToggleContainerProps) => {
   // TODO: 숫자 -> 문자열로 변경하는 부분이 많으면 util 함수로 따로 빼기
