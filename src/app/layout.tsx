90cad614248437de763257517d19ab28bc849ea4
qq8721443 <qq8721443@naver.com>
2023-06-14 23:21:18 +0900

    chore: 린트 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,11 @@
-import "./globals.css";
-import { Inter } from "next/font/google";
+import './globals.css';
+import { Inter } from 'next/font/google';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: "Jeongki Dev",
-  description: "Jeongki Dev",
+  title: 'Jeongki Dev',
+  description: 'Jeongki Dev',
 };
 
 export default function RootLayout({
